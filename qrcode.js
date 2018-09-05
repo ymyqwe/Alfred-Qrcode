f7d1b7c2a8d05d4bc997ac82315900a89e7fde23
ManiaU <yumingyuan001@lianjia.com>
2018-09-05 10:07:37 +0800

    fix empty string

--- qrcode.js
+++ qrcode.js
@@ -1,25 +1,31 @@
-const qrCode = require('qrcode');
-const text = process.argv[2]
+const qrCode = require("qrcode");
+const text = process.argv[2];
 
-const generateQR = text => (new Promise((resolve, reject) => {
-  try {
-    const result = qrCode.toFile(__dirname + "/qrcode.png", text);
-    resolve(__dirname + "/qrcode.png")
-  } catch (error) {
-    reject(error)
-  }
-}))
+const generateQR = text =>
+  new Promise((resolve, reject) => {
+    try {
+      // 空字符串的话使用上次
+      if (text.length) {
+        const result = qrCode.toFile(__dirname + "/qrcode.png", text);
+        resolve(__dirname + "/qrcode.png");
+      } else {
+        resolve("");
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
 
 generateQR(text)
-  .then((resp) => {
-    const result = resp.replace(/\s/g, "\\ ")
+  .then(resp => {
+    const result = resp.replace(/\s/g, "\\ ");
     const json = {
       title: "qrcode",
       arg: "",
       icon: {
         path: "qrcode.png"
       }
-    }
+    };
     console.log(JSON.stringify(json));
   })
-  .catch(err => console.log(err))
+  .catch(err => console.log(err));
